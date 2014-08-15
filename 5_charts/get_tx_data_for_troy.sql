39a2a86ab350f2d4a862902277649c1c4417bb45
James Gordon <gordon.je@gmail.com>
2014-08-14 23:15:27 -0500

    added top 10 states query and file, updated other csv files

--- 5_charts/get_tx_data_for_troy.sql
+++ 5_charts/get_tx_data_for_troy.sql
@@ -1,18 +1,37 @@
+-- get 10 biggest states
+SELECT
+	  a.state_code
+	, a.state_name
+	, a.emplvl_total::int / 10 as employees
+	, a.fatals_total::int / 10 as fatals
+	, ROUND(a.residual)::int / 10 as residual
+FROM states_naics2_no_agri a
+JOIN areas b
+ON a.state_code = LEFT(b.cew_code, 2)
+WHERE state_code in (
+	SELECT state_code
+	FROM states_naics2_no_agri
+	WHERE fatals_total IS NOT NULL
+	ORDER BY emplvl_total DESC
+	LIMIT 10
+)
+ORDER BY a.residual DESC;
+
 -- get Texas sector data
 SELECT 
 	  a.cew_code as naics_code
 	, COALESCE(a.display_name, a.cfoi_name) as sector_display_name
 	, a.cfoi_name as sector_full_name
 	, ROUND(b.emplvl_sum / 10) as emplvl
-	, ROUND(expect_fatals) as expected_fatals
-	, ROUND(b.residual)::int as residual
+	, b.fatals_sum / 10 as fatals
+	-- , ROUND(expect_fatals / 10) as expected_fatals
+	, ROUND(b.residual / 10)::int as residual
 	, a.sort_order
 FROM industries a
 JOIN states_industries b
 ON a.cew_code = b.industry_code
 WHERE a.display_level = 3 
 AND a.cew_code NOT LIKE '1%%'
-AND b.residual IS NOT NULL
 AND b.state_code = '48'
 ORDER BY 
 	  a.sort_order DESC;
@@ -22,12 +41,13 @@ SELECT
 	  a.cew_code as naics_code
 	, a.cfoi_name as industry
 	, ROUND(b.emplvl_sum / 10) as emplvl
-	, ROUND(expect_fatals) as expected_fatals
-	, ROUND(b.residual)::int as residual
+	, fatals_sum / 10 as fatals
+	-- , ROUND(expect_fatals / 10) as expected_fatals
+	, ROUND(b.residual / 10)::int as residual
 FROM industries a
 JOIN states_industries b
 ON a.cew_code = b.industry_code
-WHERE b.industry_code in ('23821', '23816', '23814', '23813', '23712')
+WHERE b.industry_code in ('23821', '23816', '23814', '23813')
 AND b.residual IS NOT NULL
 AND b.state_code = '48'
 ORDER BY 
@@ -38,12 +58,13 @@ SELECT
 	  a.cew_code as naics_code
 	, a.cfoi_name as industry
 	, ROUND(b.emplvl_sum / 10) as emplvl
-	, ROUND(expect_fatals) as expected_fatals
-	, ROUND(b.residual)::int as residual
+	, fatals_sum / 10 as fatals
+	-- , ROUND(expect_fatals / 10) as expected_fatals
+	, ROUND(b.residual / 10)::int as residual
 FROM industries a
 JOIN states_industries b
 ON a.cew_code = b.industry_code
-WHERE b.industry_code in ('21111', '22121', '48621', '32512', '33313', '42381', '48611')
+WHERE b.industry_code in ('21111', '22121', '48621', '32512', '33313', '42381', '48611', '23712')
 AND b.residual IS NOT NULL
 AND b.state_code = '48'
 ORDER BY 
